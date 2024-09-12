49cc73abd2d692d71b0704631611f2d84164157b
Shivane Rana <shivanerana2004@gmail.com>
2024-09-12 10:56:03 +0530

    recreated html in js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import "./style.css"
+import sushi from "./images/sushi.png";
 
-console.log("Shivane Rana made this (yay!)");
+const header = document.createElement("header");
+const nav = document.createElement("nav");
+const image = document.createElement("img");
+image.src = sushi;
+
+
+const home = document.createElement("button");
+home.textContent = "Home";
+const menu = document.createElement("button");
+menu.textContent = "Menu";
+const about = document.createElement("button");
+about.textContent = "About"; 
+const contact = document.createElement("button");
+contact.textContent = "Contact Us";
+
+nav.style.backgroundColor = "purple";
+nav.append(home,menu,about,contact);
+document.body.append(header);
+header.append(image,nav);
