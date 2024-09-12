4991c534de1b449661984b1b2e40f00be14c15a9
Shivane Rana <shivanerana2004@gmail.com>
2024-09-12 14:29:13 +0530

    all file go to hell

--- src/about.js
+++ src/about.js
@@ -1,6 +1,6 @@
 const displayHome = (function(){
     const dis = function(){
-       return "Home button was pressed and its content was loaded from another js with the help of import"; 
+       return "About button was pressed and its content was loaded from another js with the help of import"; 
     }
 
     return {dis};
